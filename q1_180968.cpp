b1212b2546f89a6715853f7be451b709b2680d6e
180968EhteshamArif <46881813+180968EhteshamArif@users.noreply.github.com>
2019-02-04 23:01:52 +0500

    Add files via upload

--- q1_180968.cpp
+++ q1_180968.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<fstream>
-#include "Header2.h"
+#include "Header.h"
 #include<fstream>
 #include<string>
 using namespace std;
@@ -23,57 +23,57 @@ void Display(gitHubUser* arr, int size)
 //function to print educational background
 void print(gitHubUser &user)
 {
-	cout<<"The name of educational institution of the user is :"<<user.institute_name<<endl;
-	cout<<"The qualification level of the user is :"<<user.qualification_level<<endl;
+	cout<<"The name of educational institution of the user is :"<<*user.institute_name<<endl;
+	cout<<"The qualification level of the user is :"<<*user.qualification_level<<endl;
 }
 //function to set educational background
 void setEduBckGrnd(gitHubUser &user)
 {
-	int input;
-	int input2;
+	char input;
+	char input2;
 
-	      cout<<"Enter your Institusion Name (Optional), Press y for 'Yes' or n for 'No'"<<endl;
+	      cout<<"Enter your Educational Background (Optional), Press y for 'Yes' or n for 'No'"<<endl;
 	      cin>>input;
 	      if(input=='y'|| input=='Y')
 	      {
-			  cout<<"Now Enter the name :"<<endl;
+			  cout<<"Now Enter the Institutional name :";
 			  user.institute_name=new string;
 			  cin>>*user.institute_name;
 			 // getline(cin,*user.institute_name);
+			  cout<<"Now Enter the Qualification Level :";
+			  user.qualification_level=new string;
+			  cin>>*user.qualification_level;
+			 // getline(cin,*user.qualification_level);   
 	      }
-		  else if(input=='N'||input=='n')
+		  else
 		  {
-			  cout<<"Enter your Qualification Level (Optional), press y for 'Yes' or n for 'No'"<<endl;
-			  cin>>input2;
-			   if(input2=='y'|| input2=='Y')
-	          {
-				  user.qualification_level=new string;
-
-				  cin>>*user.qualification_level;
-				 // getline(cin,*user.qualification_level);
-	          }
-			   else if(input2=='N'|| input2=='n')
-			   {
-				   cout<<"-----That's it-----"<<endl;  
-			   }
+			  cout<<"-----That's it-----"<<endl;
 		  }
 }
 //function to remove educational background for the user
 void remove(gitHubUser &user)
 {
-	int input;
+	char input;
 	cout<<"Do you want to remove the educational background ?"<<" Press y for 'Yes' and n for 'N' :";
 	cin>>input;
-	if(input=='y'|| input=='Y')
+	if(user.qualification_level!=nullptr||user.institute_name!=nullptr)
 	{
-		delete[] user.qualification_level;
-		delete[] user.institute_name;
-		user.qualification_level=nullptr;
-		user.institute_name=nullptr;
+	    if(input=='y'|| input=='Y')
+	    {
+	 	    delete[] user.qualification_level;
+		    delete[] user.institute_name;
+		    user.qualification_level=nullptr;
+		    user.institute_name=nullptr;
+	    }
+		else
+		{
+	    cout<<"You opt to not to delete your information "<<endl;
+		}
 	}
-	if(input=='N'||input=='n')
+	else
 	{
-	cout<<"You opt to not to delete your information "<<endl;
+		cout<<"---No information available---"<<endl;
+	
 	}
 }
 //fucntion to read data
@@ -121,7 +121,6 @@ void backup(gitHubUser *originalArry, gitHubUser *&backupArry, int userCount)
 		{
 			backupArry[i].gitHubFolders=new string[originalArry[i].foldercount];
 			backupArry[i].gitHubFolders[j]=originalArry[i].gitHubFolders[j];
-			backupArry[i].gitHubFolders=new string;
 			backupArry[i].institute_name=originalArry[i].institute_name;
 			backupArry[i].qualification_level=new string;
 			backupArry[i].qualification_level=originalArry[i].qualification_level;
@@ -131,24 +130,47 @@ void backup(gitHubUser *originalArry, gitHubUser *&backupArry, int userCount)
 //respective main
 int main()
 {
-	gitHubUser arr[2];
+	char input;
+	int input2;
+	const int size=2;
+	gitHubUser arr[size];
 	gitHubUser *backUp;
 	string filepath="save.txt";
 	int user;
-	int i;
 	readDataFromFile(arr,filepath);
-	cout<<"Enter the user you want to set Educational background with (1 or 2):";
+	cout<<"Enter the user you want to set Educational background (Simple 1,2,3) :"<<endl;
 	cin>>user;
-	if(user==1)
+	user--;
+	if(user==1||user==0)
 	{
-		 i=0;
+		setEduBckGrnd(arr[user]);
+		print(arr[user]);
 	}
-	if(user==2)
+	else
 	{
-		i=1;
+	  cout<<" You Entered the wrong user!"<<endl;
 	}
-	setEduBckGrnd(arr[1]);
-	print(arr[1]);
-	remove(arr[1]);
+	cout<<"Enter the number of User for which you want to remove Educational Background :(Simple 1,2,3) "<<endl;
+	cin>>input2;
+	input2--;
+	if(input2==1||input2==0)
+	{
+		remove(arr[input2]);
+	}
+	else
+	{
+		cout<<" You Entered the wrong user!"<<endl;
+	}
+	cout<<"Do you want to make backup of your profile? Press y for 'Yes' and n for 'No'"<<endl;
+	cin>>input;
+	if(input=='Y'||input=='y')
+	{
 	backup(arr,backUp,2);
+	}
+	else
+	{
+		cout<<"---You chose to Exit--"<<endl;
+	}
+	system("pause");
+	return 0;
 }
